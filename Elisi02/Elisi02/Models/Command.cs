371a3ba54dfdd27170a0d4db8eb0b8866c11e5ae
iRumba <reloader2003@gmail.com>
2017-05-26 10:27:52 +0700

    Небольшой клинап

--- Elisi02/Elisi02/Models/Command.cs
+++ Elisi02/Elisi02/Models/Command.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace Elisi02.Models
 {
     [XmlType(TypeName = "command")]
-    //[Serializable]
     public class Command
     {
         [XmlIgnore]
